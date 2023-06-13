4bd2f1ced1c8373758f6a7bedda110abb8d63220
vince <vince@forevernine.com>
2023-06-13 21:56:26 +0800

    feat：new brick

--- code/script/Brick.cs
+++ code/script/Brick.cs
@@ -7,9 +7,20 @@ public class Brick : Node2D
     // private int a = 2;
     // private string b = "text";
 
+    [Export]
+    public int _hlt = 1;
+
+    [Export]
+    public NodePath _hltPath;
+
+    StaticBody2D _staticBody2D;
+
+    public Label _hltLabel;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _hltLabel = GetNode<Label>(_hltPath);
 
     }
 
@@ -24,8 +35,8 @@ public class Brick : Node2D
     //
     //  }
 
-    public void free()
-    {
-        GetParent().GetParent().QueueFree();
-    }
+    // public void free()
+    // {
+    //     GetParent().GetParent().QueueFree();
+    // }
 }
